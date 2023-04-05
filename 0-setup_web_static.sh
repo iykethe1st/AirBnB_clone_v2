f3d3e210edf8eb19df435bd5ec3c9bb5ebcdbe08
Ikenna <iykethe1st@gmail.com>
2023-04-05 11:18:37 -0700

    preparing the web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Script sets up a web server for deployment of web_static.
 
 apt-get update
 apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
 chgrp -R ubuntu /data/
+chown -R ubuntu /data/
 
 printf %s "server {
     listen 80 default_server;
